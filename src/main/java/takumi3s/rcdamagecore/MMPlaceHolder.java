9adb0f36fca2f2b3272ace33f4030c843395136c
sysnote8main <sysnote8main@gmail.com>
2025-02-24 11:43:49 +0900

    chore: refactoring... (3/n)

--- src/main/java/takumi3s/rcdamagecore/MMPlaceHolder.java
+++ src/main/java/takumi3s/rcdamagecore/MMPlaceHolder.java
@@ -10,33 +10,40 @@ import org.bukkit.event.Listener;
 import java.util.UUID;
 
 public class MMPlaceHolder implements Listener {
+    private static final String[] types = new String[]{
+            "none",
+            "fire",
+            "ice",
+            "wind",
+            "lightning",
+            "rekketu",
+            "syukusei",
+            "critper",
+            "critdmg"
+    };
+
     @EventHandler
-    public static String MMPlaceHolder(MythicReloadedEvent e) {
+    public static String setMMPlaceHolder(MythicReloadedEvent e) {
         PlaceholderManager pm = MythicBukkit.inst().getPlaceholderManager();
+
         String buff = "0";
-        buff = phBuffer("none", pm, buff);
-        buff = phBuffer("fire", pm, buff);
-        buff = phBuffer("ice", pm, buff);
-        buff = phBuffer("wind", pm, buff);
-        buff = phBuffer("lightning", pm, buff);
-        buff = phBuffer("rekketu", pm, buff);
-        buff = phBuffer("syukusei", pm, buff);
-        buff = phBuffer("critper", pm, buff);
-        buff = phBuffer("critdmg", pm, buff);
+        for(String type: types) {
+            buff = phBuffer(type, pm, buff);
+        }
+
         return buff;
     }
 
     public static String phBuffer(String type, PlaceholderManager pm, String buff) {
-
         pm.register("caster.buff." + type, Placeholder.meta((meta, s) -> {
             UUID uuid = meta.getTrigger().getUniqueId();
-            String[] sp = s.split("\\.");
             if (RcBuff.map.containsKey(uuid) && RcBuff.map.get(uuid).containsKey(type)) {
                 return String.valueOf(RcBuff.map.get(uuid).get(type) + RcBuff.armorBuff(uuid, type));
             } else {
                 return buff;
             }
         }));
+
         return buff;
     }
 
