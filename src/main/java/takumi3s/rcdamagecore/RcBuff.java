2c51bd929c887852b2acb2c4dce7b2cb6bfa7cbc
sysnote8main <sysnote8main@gmail.com>
2025-02-24 11:19:22 +0900

    chore: move data extractor to other class

--- src/main/java/takumi3s/rcdamagecore/RcBuff.java
+++ src/main/java/takumi3s/rcdamagecore/RcBuff.java
@@ -12,12 +12,8 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.EntityEquipment;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import takumi3s.rcdamagecore.util.ItemTypeUtil;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -45,26 +41,13 @@ public class RcBuff implements ISkillMechanic, ITargetedEntitySkill {
 
         EntityEquipment equipment = entity.getEquipment();
 
-        return extractTypeDataFromAll(
+        return ItemTypeUtil.extractTypeDataFromAll(
                 key,
                 equipment.getHelmet(),
                 equipment.getChestplate(),
                 equipment.getLeggings(),
                 equipment.getBoots()
         );
-    }
-
-    public static int extractTypeDataFromAll(@NotNull NamespacedKey key, @Nullable ItemStack ...stacks) {
-        return Arrays.stream(stacks).mapToInt(s -> extractTypeData(key, s)).sum();
-    }
-
-    public static int extractTypeData(@NotNull NamespacedKey key, @Nullable ItemStack stack) {
-        if(stack != null && stack.hasItemMeta()) {
-            return stack.getItemMeta()
-                    .getPersistentDataContainer()
-                    .getOrDefault(key, PersistentDataType.INTEGER, 0);
-        }
-        return 0;
     }
 
     //amountの量のtypeのバフをduration時間する。
