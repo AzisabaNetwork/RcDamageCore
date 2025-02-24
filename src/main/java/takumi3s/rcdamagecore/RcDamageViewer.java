cffd5266869c39d52d08f3354e3aa3311a88c034
sysnote8main <sysnote8main@gmail.com>
2025-02-24 11:21:01 +0900

    chore: extract location as field

--- src/main/java/takumi3s/rcdamagecore/RcDamageViewer.java
+++ src/main/java/takumi3s/rcdamagecore/RcDamageViewer.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -20,13 +21,15 @@ public class RcDamageViewer implements Listener {
         ProtocolManager pm = ProtocolLibrary.getProtocolManager();
         PacketContainer pc = pm.createPacket(PacketType.Play.Server.SPAWN_ENTITY);
 
+        Location location = e.getEntity().getLocation();
+
         pc.getModifier().writeDefaults();
         pc.getEntityTypeModifier().write(0, EntityType.ARMOR_STAND);
         pc.getUUIDs().write(0, UUID.randomUUID());
         pc.getIntegers().write(1, 1);
-        pc.getDoubles().write(0, e.getEntity().getLocation().getX() + (Math.random() * 6) - 3);
-        pc.getDoubles().write(1, e.getEntity().getLocation().getY() + (Math.random()) + 2);
-        pc.getDoubles().write(2, e.getEntity().getLocation().getZ() + (Math.random() * 6) - 3);
+        pc.getDoubles().write(0, location.getX() + (Math.random() * 6) - 3);
+        pc.getDoubles().write(1, location.getY() + (Math.random()) + 2);
+        pc.getDoubles().write(2, location.getZ() + (Math.random() * 6) - 3);
 
         PacketContainer pc2 = pm.createPacket(PacketType.Play.Server.ENTITY_METADATA);
 
