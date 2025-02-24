8c5e2941059bba6665808d5d9624fbf9a0586ac8
sysnote8main <sysnote8main@gmail.com>
2025-02-24 11:40:02 +0900

    chore: refactoring... (2/n)

--- src/main/java/takumi3s/rcdamagecore/RcDamageListener.java
+++ src/main/java/takumi3s/rcdamagecore/RcDamageListener.java
@@ -9,7 +9,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.potion.PotionEffectType;
+import takumi3s.rcdamagecore.util.ArmorDataUtil;
+import takumi3s.rcdamagecore.util.AttributeUtil;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -21,64 +24,54 @@ public class RcDamageListener implements Listener {
         UUID uuid = e.getDamager().getUniqueId();
 
 
-        //ダメージ受けた人の防具値防具強度値取得する。
+        // ダメージを受けた人のデータ取得
         double armorToughness = 0;
         double armor = 0;
         double armorEc = 0;
-
-        if (e.getEntity() instanceof LivingEntity damaged) {
-            if (damaged.getAttribute(Attribute.GENERIC_ARMOR) != null) {
-                armor = Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR)).getValue();
-            }
-            if (damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS) != null) {
-                armorToughness = Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS)).getValue();
-            }
-            if (damaged.getEquipment().getHelmet() != null) {
-                armorEc += damaged.getEquipment().getHelmet().getEnchantmentLevel(Enchantment.PROTECTION);
-            }
-            if (damaged.getEquipment().getChestplate() != null) {
-                armorEc += damaged.getEquipment().getChestplate().getEnchantmentLevel(Enchantment.PROTECTION);
-            }
-            if (damaged.getEquipment().getLeggings() != null) {
-                armorEc += damaged.getEquipment().getLeggings().getEnchantmentLevel(Enchantment.PROTECTION);
-            }
-            if (damaged.getEquipment().getBoots() != null) {
-                armorEc += damaged.getEquipment().getBoots().getEnchantmentLevel(Enchantment.PROTECTION);
-            }
-        }
-
-        //ダメージ与えた人の攻撃力取得する。
-        double attack = 0;
-
-        if (e.getDamager() instanceof LivingEntity damager) {
-            if (damager.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE) != null) {
-                attack = Objects.requireNonNull(damager.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE)).getValue();
-            }
-        }
-
-        //ダメージ受けた人の耐性効果を取得する。
         double resistance = 0;
+
         if (e.getEntity() instanceof LivingEntity damaged) {
+            armor = AttributeUtil.getOrZero(damaged, Attribute.GENERIC_ARMOR);
+            armorToughness = AttributeUtil.getOrZero(damaged, Attribute.GENERIC_ARMOR_TOUGHNESS);
+
+            EntityEquipment equipment = damaged.getEquipment();
+            if(equipment != null) {
+                armorEc += ArmorDataUtil.getProtectionValueFromAll(
+                        equipment.getHelmet(),
+                        equipment.getChestplate(),
+                        equipment.getLeggings(),
+                        equipment.getBoots()
+                );
+            }
+
+            // 耐性効果
             if (damaged.hasPotionEffect(PotionEffectType.RESISTANCE)) {
                 resistance = (Objects.requireNonNull(damaged.getPotionEffect(PotionEffectType.RESISTANCE)).getAmplifier() + 1);
             }
+        }
+
+        // ダメージを与えた人のデータ取得
+        double attack = 0;
+
+        if (e.getDamager() instanceof LivingEntity damager) {
+            // 攻撃力
+            attack = AttributeUtil.getOrZero(damager, Attribute.GENERIC_ATTACK_DAMAGE);
         }
 
         //クリティカルの計算。
         int critValue = (int) (Math.random() * 1000);
         int critPer = 10 + RcBuff.armorBuff(uuid, "critper");
         double critDamage = 5 + RcBuff.armorBuff(uuid, "critdmg");
+
+        // 全体へダメージ値を送信
         Bukkit.broadcast(Component.text(critDamage));
+
+        // バフがある場合
         if (RcBuff.map.containsKey(uuid)) {
-            if (RcBuff.map.get(uuid).containsKey("critper")) {
-                critPer += RcBuff.map.get(uuid).get("critper");
-            }
+            critPer += RcBuff.map.get(uuid).getOrDefault("critper", 0);
+            critDamage += RcBuff.map.get(uuid).getOrDefault("critdmg", 0);
         }
-        if (RcBuff.map.containsKey(uuid)) {
-            if (RcBuff.map.get(uuid).containsKey("critdmg")) {
-                critDamage += RcBuff.map.get(uuid).get("critdmg");
-            }
-        }
+
         if (critPer >= critValue) {
             damage = damage * (1 + (critDamage / 1000));
         }
@@ -100,10 +93,11 @@ public class RcDamageListener implements Listener {
 
         if (finalDamage <= 0) {
             e.setCancelled(true);
-        } else {
-            e.setDamage(0.0);
-            e.setDamage(EntityDamageEvent.DamageModifier.ARMOR, finalDamage);
+            return;
         }
+
+        e.setDamage(0.0);
+        e.setDamage(EntityDamageEvent.DamageModifier.ARMOR, finalDamage);
 
     }
 
