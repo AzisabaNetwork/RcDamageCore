d36d96f3e8f3b60a68092125f956465dc1c95945
sysnote8main <sysnote8main@gmail.com>
2025-03-02 09:23:17 +0900

    feat: add ACF for easy command building

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,19 @@
 plugins {
     `java-library`
-    `maven-publish`
+    alias(libs.plugins.shadow)
 }
 
 repositories {
     mavenLocal()
-    maven {
-        url = uri("https://repo.dmulloy2.net/repository/public/")
-    }
-
-    maven {
-        url = uri("https://mvn.lumine.io/repository/maven-public/")
-    }
-
-    maven {
-        url = uri("https://oss.sonatype.org/content/groups/public/")
-    }
-
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
-
-    maven {
-        url = uri("https://repo.azisaba.net/repository/maven-public/")
-    }
-
-    maven {
-        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    }
-
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
-
+    maven("https://repo.dmulloy2.net/repository/public/")
+    maven("https://mvn.lumine.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://repo.azisaba.net/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.maven.apache.org/maven2/")
     maven("https://repo.papermc.io/repository/maven-public/")
-}
-
-dependencies {
-    compileOnly(libs.paper.api)
-    compileOnly(libs.protocollib)
-    compileOnly(libs.mythic.dist)
-    compileOnly("net.azisaba.loreeditor:api:${libs.versions.loreeditor.api.get()}:all")
+    maven("https://repo.aikar.co/content/groups/aikar/")
 }
 
 group = "takumi3s.rcdamagecore"
@@ -48,10 +21,19 @@ version = "0.1.0-SNAPSHOT"
 description = "RcDamageCore"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
+dependencies {
+    compileOnly(libs.paper.api)
+    compileOnly(libs.protocollib)
+    compileOnly(libs.mythic.dist)
+    compileOnly("net.azisaba.loreeditor:api:${libs.versions.loreeditor.api.get()}:all")
+
+    implementation(libs.acf.paper)
+}
+
+tasks.compileJava {
+    options.isFork = true
+    options.compilerArgs.add("-parameters")
+    options.forkOptions.executable = System.getProperty("java.home") + "/bin/javac"
 }
 
 tasks.withType<JavaCompile> {
@@ -60,4 +42,9 @@ tasks.withType<JavaCompile> {
 
 tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
+}
+
+tasks.shadowJar {
+    relocate("co.aikar.commands", "takumi3s.rcdamagecore.acf")
+    relocate("co.aikar.locales", "takumi3s.rcdamagecore.locales")
 }
