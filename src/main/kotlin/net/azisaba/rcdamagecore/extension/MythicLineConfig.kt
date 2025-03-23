216249cb3ef157ac7151e5ad6e883ae6b2261e18
sysnote8main <sysnote8main@gmail.com>
2025-03-23 21:20:13 +0900

    feat: set explicit type for getStr

--- src/main/kotlin/net/azisaba/rcdamagecore/extension/MythicLineConfig.kt
+++ src/main/kotlin/net/azisaba/rcdamagecore/extension/MythicLineConfig.kt
@@ -2,6 +2,6 @@ package net.azisaba.rcdamagecore.extension
 
 import io.lumine.mythic.api.config.MythicLineConfig
 
-fun MythicLineConfig.getStr(vararg aliases: String) = getString(aliases)
+fun MythicLineConfig.getStr(vararg aliases: String): String? = getString(aliases)
 
 fun MythicLineConfig.getInt(vararg aliases: String) = getInteger(aliases)
