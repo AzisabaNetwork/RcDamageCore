81cfe74cef8e0718c1fddacf10be6a1afc43f630
sysnote8main <sysnote8main@gmail.com>
2025-03-23 21:17:00 +0900

    feat: remove unused codes.

--- src/main/java/takumi3s/rcdamagecore/RDCVariables.java
+++
@@ -1,5 +0,0 @@
-package takumi3s.rcdamagecore;
-
-public class RDCVariables {
-    public static final String ID = "rcdamagecore";
-}
--- src/main/java/takumi3s/rcdamagecore/util/LevelData.java
+++
@@ -1,7 +0,0 @@
-package takumi3s.rcdamagecore.util;
-
-public record LevelData(
-        int exp,
-        int itemLevel,
-        int levelChangeAmount
-) {}
